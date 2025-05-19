fbbe5965dde19a9c30c63bb25d58bd6adfcfa4fa
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2025-05-19 17:50:05 -0400

    some adjustment

--- fragment.glsl
+++ fragment.glsl
@@ -4,8 +4,8 @@ uniform vec2 u_mouse;
 uniform vec2 u_prevMouse;
 
 void main() {
-    vec2 gridUV = floor(vUv * vec2(24.0, 24.0)) / vec2(24.0, 24.0);
-    vec2 centerOfPixel = gridUV + vec2(24.0/444.0, 24.0/444.0);
+    vec2 gridUV = floor(vUv * vec2(44.0, 44.0)) / vec2(44.0, 44.0);
+    vec2 centerOfPixel = gridUV + vec2(44.0/11144.0, 44.0/11144.0);
 
     vec2 mouseDirection = u_mouse - u_prevMouse;
 
@@ -13,7 +13,7 @@ void main() {
     float pixelDistanceToMouse = length(pixelToMouseDirection);
     float strength = smoothstep(0.3, 0.0001, pixelDistanceToMouse);
 
-    vec2 uvOffset = strength * -mouseDirection * 0.14;
+    vec2 uvOffset = strength * -mouseDirection * 0.54;
     vec2 uv = vUv - uvOffset;
 
     vec4 color = texture2D(u_texture, uv);
